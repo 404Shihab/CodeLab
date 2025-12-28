39a286c251d60a801bb4ce0ca3d1afc73d8c0c6a
404Shihab <shihabbhuiyan500@gmail.com>
2025-12-28 21:35:46 +0600

    updated

--- CodeLab/LogIn.Designer.cs
+++ CodeLab/LogIn.Designer.cs
@@ -28,16 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogIn));
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.ulbl = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.rightPnl = new System.Windows.Forms.Panel();
-            this.utxt = new System.Windows.Forms.TextBox();
-            this.passlbl = new System.Windows.Forms.Label();
-            this.passtxt = new System.Windows.Forms.TextBox();
+            this.signUpLinklbl = new System.Windows.Forms.LinkLabel();
+            this.qlbl = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.passtxt = new System.Windows.Forms.TextBox();
+            this.passlbl = new System.Windows.Forms.Label();
+            this.utxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logoimg = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.rightPnl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoimg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -45,28 +52,15 @@
             this.label1.AllowDrop = true;
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Blue;
-            this.label1.Location = new System.Drawing.Point(94, 140);
+            this.label1.Location = new System.Drawing.Point(93, 185);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(325, 57);
             this.label1.TabIndex = 0;
             this.label1.Text = "Wellcome Back";
             this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AllowDrop = true;
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(96, 239);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(183, 41);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Log in now!";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // ulbl
             // 
@@ -83,15 +77,17 @@
             // panel1
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.BackColor = System.Drawing.Color.Blue;
-            this.panel1.Location = new System.Drawing.Point(0, 691);
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Location = new System.Drawing.Point(0, 666);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1110, 27);
+            this.panel1.Size = new System.Drawing.Size(1110, 52);
             this.panel1.TabIndex = 3;
             // 
             // rightPnl
             // 
             this.rightPnl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.rightPnl.Controls.Add(this.signUpLinklbl);
+            this.rightPnl.Controls.Add(this.qlbl);
             this.rightPnl.Controls.Add(this.button1);
             this.rightPnl.Controls.Add(this.passtxt);
             this.rightPnl.Controls.Add(this.passlbl);
@@ -99,42 +95,35 @@
             this.rightPnl.Controls.Add(this.ulbl);
             this.rightPnl.Controls.Add(this.label2);
             this.rightPnl.Controls.Add(this.label1);
-            this.rightPnl.Location = new System.Drawing.Point(214, 21);
+            this.rightPnl.Controls.Add(this.pictureBox1);
+            this.rightPnl.Location = new System.Drawing.Point(519, 12);
             this.rightPnl.Name = "rightPnl";
             this.rightPnl.Size = new System.Drawing.Size(559, 648);
             this.rightPnl.TabIndex = 4;
             // 
-            // utxt
+            // signUpLinklbl
             // 
-            this.utxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.utxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.utxt.ForeColor = System.Drawing.Color.Blue;
-            this.utxt.Location = new System.Drawing.Point(103, 349);
-            this.utxt.Name = "utxt";
-            this.utxt.Size = new System.Drawing.Size(339, 30);
-            this.utxt.TabIndex = 3;
+            this.signUpLinklbl.AutoSize = true;
+            this.signUpLinklbl.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signUpLinklbl.Location = new System.Drawing.Point(360, 573);
+            this.signUpLinklbl.Name = "signUpLinklbl";
+            this.signUpLinklbl.Size = new System.Drawing.Size(82, 28);
+            this.signUpLinklbl.TabIndex = 8;
+            this.signUpLinklbl.TabStop = true;
+            this.signUpLinklbl.Text = "Sign up";
+            this.signUpLinklbl.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
-            // passlbl
+            // qlbl
             // 
-            this.passlbl.AllowDrop = true;
-            this.passlbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.passlbl.AutoSize = true;
-            this.passlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passlbl.Location = new System.Drawing.Point(98, 402);
-            this.passlbl.Name = "passlbl";
-            this.passlbl.Size = new System.Drawing.Size(98, 25);
-            this.passlbl.TabIndex = 4;
-            this.passlbl.Text = "Password";
-            // 
-            // passtxt
-            // 
-            this.passtxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.passtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passtxt.ForeColor = System.Drawing.Color.Blue;
-            this.passtxt.Location = new System.Drawing.Point(103, 430);
-            this.passtxt.Name = "passtxt";
-            this.passtxt.Size = new System.Drawing.Size(339, 30);
-            this.passtxt.TabIndex = 5;
+            this.qlbl.AllowDrop = true;
+            this.qlbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.qlbl.AutoSize = true;
+            this.qlbl.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.qlbl.Location = new System.Drawing.Point(98, 573);
+            this.qlbl.Name = "qlbl";
+            this.qlbl.Size = new System.Drawing.Size(235, 28);
+            this.qlbl.TabIndex = 7;
+            this.qlbl.Text = "Dont have an account ?";
             // 
             // button1
             // 
@@ -150,20 +139,89 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // passtxt
+            // 
+            this.passtxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.passtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passtxt.ForeColor = System.Drawing.Color.Blue;
+            this.passtxt.Location = new System.Drawing.Point(103, 430);
+            this.passtxt.Name = "passtxt";
+            this.passtxt.Size = new System.Drawing.Size(339, 30);
+            this.passtxt.TabIndex = 5;
+            // 
+            // passlbl
+            // 
+            this.passlbl.AllowDrop = true;
+            this.passlbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.passlbl.AutoSize = true;
+            this.passlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passlbl.Location = new System.Drawing.Point(98, 402);
+            this.passlbl.Name = "passlbl";
+            this.passlbl.Size = new System.Drawing.Size(98, 25);
+            this.passlbl.TabIndex = 4;
+            this.passlbl.Text = "Password";
+            // 
+            // utxt
+            // 
+            this.utxt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.utxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.utxt.ForeColor = System.Drawing.Color.Blue;
+            this.utxt.Location = new System.Drawing.Point(103, 349);
+            this.utxt.Name = "utxt";
+            this.utxt.Size = new System.Drawing.Size(339, 30);
+            this.utxt.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(96, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Log in now!";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // logoimg
+            // 
+            this.logoimg.BackColor = System.Drawing.Color.Transparent;
+            this.logoimg.Image = ((System.Drawing.Image)(resources.GetObject("logoimg.Image")));
+            this.logoimg.Location = new System.Drawing.Point(8, 130);
+            this.logoimg.Name = "logoimg";
+            this.logoimg.Size = new System.Drawing.Size(505, 472);
+            this.logoimg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoimg.TabIndex = 5;
+            this.logoimg.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-206, -80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(952, 398);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
             // LogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(1090, 720);
+            this.Controls.Add(this.logoimg);
             this.Controls.Add(this.rightPnl);
             this.Controls.Add(this.panel1);
             this.Name = "LogIn";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CodeLab Log In";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.LogIn_Load);
             this.rightPnl.ResumeLayout(false);
             this.rightPnl.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoimg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -171,7 +229,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label ulbl;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel rightPnl;
@@ -179,6 +236,11 @@
         private System.Windows.Forms.TextBox passtxt;
         private System.Windows.Forms.Label passlbl;
         private System.Windows.Forms.TextBox utxt;
+        private System.Windows.Forms.PictureBox logoimg;
+        private System.Windows.Forms.LinkLabel signUpLinklbl;
+        private System.Windows.Forms.Label qlbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
---
+++ CodeLab/SignUp.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace CodeLab
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 25.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(587, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(434, 57);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sing Up for CodeLab";
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1087, 739);
+            this.Controls.Add(this.label2);
+            this.Name = "SignUp";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "CodeLab SignUp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+    }
+}
