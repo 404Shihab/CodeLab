c9b1fc0bb81e80cf050c1b5423d4dbabcdae4130
404Shihab <shihabbhuiyan500@gmail.com>
2026-01-05 13:22:39 +0600

    Log in page updated

--- CodeLab/LogIn.cs
+++ CodeLab/LogIn.cs
@@ -40,7 +40,63 @@ namespace CodeLab
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            string username = utxt.Text.Trim();
+            string password = passtxt.Text.Trim();
+
+            // ===== ADMIN LOGIN =====
+            if (username == "admin1" && password == "adminpass")
+            {
+                AdminDashboard ad = new AdminDashboard();
+                ad.Show();
+                this.Hide();
+                return;
+            }
+
+            string conStr = @"Data Source=(LocalDB)\MSSQLLocalDB;
+    AttachDbFilename=D:\C# Codes\CodeLab\CodeLab\CodeLabdb.mdf;
+    Integrated Security=True;
+    Connect Timeout=30";
+
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+
+                
+                SqlCommand cmdInstructor = new SqlCommand(
+                    "SELECT COUNT(*) FROM InstructorsInfo WHERE UserName=@u AND Password=@p", con);
+
+                cmdInstructor.Parameters.AddWithValue("@u", username);
+                cmdInstructor.Parameters.AddWithValue("@p", password);
+
+                int instructor = (int)cmdInstructor.ExecuteScalar();
+
+                if (instructor > 0)
+                {
+                    InstructorDashboard id = new InstructorDashboard();
+                    id.Show();
+                    this.Hide();
+                    return;
+                }
+
+                
+                SqlCommand cmdLearner = new SqlCommand(
+                    "SELECT COUNT(*) FROM LearnersInfo WHERE UserName=@u AND Password=@p", con);
+
+                cmdLearner.Parameters.AddWithValue("@u", username);
+                cmdLearner.Parameters.AddWithValue("@p", password);
+
+                int learner = (int)cmdLearner.ExecuteScalar();
+
+                if (learner > 0)
+                {
+                    LearnerDashboard ld = new LearnerDashboard();
+                    ld.Show();
+                    this.Hide();
+                    return;
+                }
+
+                MessageBox.Show("Invalid Username or Password");
+            }
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
